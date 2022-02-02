228830ea59155037842404057486ccb8c02bfc04
Andrea77g <91381365+Andrea77g@users.noreply.github.com>
2022-02-02 22:22:43 +0100

    Update somma-e-media.js

--- esercizi-stringhe-numeri/somma-e-media.js
+++ esercizi-stringhe-numeri/somma-e-media.js
@@ -8,3 +8,15 @@
   
   http://www.imparareaprogrammare.it
 */
+let a =[];
+
+for (i=0;i<5;i++) {a[i] = parseInt(prompt('INSERISCI IL ' + (i+1) + '° NUMERO'));};
+
+let sum;
+function somma(a){
+    sum=a.reduce( (x,y)=> x+y);
+    return sum;
+}; 
+sum = somma(a);
+
+console.log(`LA SOMMA DEL NUMERO E': ${sum} , MENTRE LA SUA MEDIA E' ${sum/a.length}`);
