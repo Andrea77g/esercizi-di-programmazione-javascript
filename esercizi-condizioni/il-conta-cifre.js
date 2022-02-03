859d270160975a2f2a549b3522821fd95aaef1ce
Andrea77g <91381365+Andrea77g@users.noreply.github.com>
2022-02-03 22:36:48 +0100

    Update il-conta-cifre.js

--- esercizi-condizioni/il-conta-cifre.js
+++ esercizi-condizioni/il-conta-cifre.js
@@ -11,3 +11,18 @@
 
   http://www.imparareaprogrammare.it
 */
+let choice = parseInt(prompt('Scegli un numero tra -9999 e 9999'));
+
+if (choice>-10000 && choice<10000) {
+    
+    //console.log('ok')
+
+    if (choice<0 && choice >-10000){choice*=-1;} //trasforma numeri negativi in positivi
+    //console.log(choice);
+
+    if (choice<10) {console.log('Il numero immesso ha una cifra')}
+    else if (choice>=10 && choice<100) {console.log('Il numero immesso ha due cifre')}
+    else if (choice>=100 && choice<1000) {console.log('Il numero immesso ha tre cifre')}
+    else if (choice>=1000 && choice<10000) {console.log('Il numero immesso ha quattro cifre')}
+}
+else {console.log('Hai digitato un valore errato!');}
