7fe8861e6e1b0aa1d4895fbb3d93af8a8141e663
Andrea77g <91381365+Andrea77g@users.noreply.github.com>
2022-02-03 21:52:12 +0100

    Update menu-dei-dolci.js

--- esercizi-condizioni/menu-dei-dolci.js
+++ esercizi-condizioni/menu-dei-dolci.js
@@ -21,3 +21,23 @@
 
   http://www.imparareaprogrammare.it
 */
+const menu = ['1. Tiramisù', '2. Torta della nonna', '3. Cheesecake alla nutella', '4. Macedonia'];
+console.log(`${menu[0]}\n${menu[1]}\n${menu[2]}\n${menu[3]}`);
+let choice = parseInt(prompt('Scegli un dolce tra quelli disponibili da 1 a 4'));
+
+if (choice<1 || choice>4) {console.log('Scelta non disponibile');}
+else { switch (choice) {
+       case 1: 
+       console.log('Hai scelto il dolce Tiramisù');
+       break;
+       case 2: 
+       console.log('Hai scelto il dolce Torta della nonna');
+       break;
+       case 3: 
+       console.log('Hai scelto il dolce Cheesecake alla nutella');
+       break;
+       case 4: 
+       console.log('Hai scelto il dolce Macedonia');
+       break;
+    }
+}
