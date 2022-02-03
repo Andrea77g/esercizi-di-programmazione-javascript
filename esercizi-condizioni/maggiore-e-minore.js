01f3225f594cb22d6d3e5ac00a34f148d08da4f0
Andrea77g <91381365+Andrea77g@users.noreply.github.com>
2022-02-03 21:19:06 +0100

    Update maggiore-e-minore.js

--- esercizi-condizioni/maggiore-e-minore.js
+++ esercizi-condizioni/maggiore-e-minore.js
@@ -10,3 +10,40 @@
 
   http://www.imparareaprogrammare.it
 */
+
+/*let a =[];
+
+for (i=0;i<4;i++) {a[i] = parseInt(prompt('INSERISCI IL ' + (i+1) + '° NUMERO'));};
+console.log(a);
+
+let maggiore = (a)=>Math.max(...a);
+let minore = (a)=>Math.min(...a);
+
+console.log('il numero maggiore è: ' + maggiore(a));
+console.log('il numero minore è: ' + minore(a));
+*/
+
+let a =[];
+
+for (i=0;i<4;i++) {a[i] = parseInt(prompt('INSERISCI IL ' + (i+1) + '° NUMERO'));};
+console.log(a);
+
+function compare(a){ 
+let max, min, compare; 
+  
+    if(a[0]>a[1]) {max = a[0]; min = a[1];}
+    else {max = a[1]; min = a[0];}
+
+    if(a[2]>max) {compare=max; max=a[2];}
+    else if(a[2]<min) {compare=min; min=a[2];}
+    else {compare=a[2];}
+
+    if(a[3]>max) {compare=max; max=a[3];}
+    else if(a[3]<min) {compare=min; min=a[3];}
+    else {compare=a[3];}
+ 
+    console.log('il numero maggiore è: ' + max);
+    console.log('il numero minore è: ' + min);
+};
+
+compare(a);
