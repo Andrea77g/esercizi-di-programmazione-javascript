abf059d71bb9085ca75e42f4043eb9e7a70ba5c9
Andrea77g <91381365+Andrea77g@users.noreply.github.com>
2022-02-05 00:44:38 +0100

    Update fai-il-professore.js

--- esercizi-condizioni/fai-il-professore.js
+++ esercizi-condizioni/fai-il-professore.js
@@ -18,3 +18,50 @@
 
   http://www.imparareaprogrammare.it
 */
+
+/*
+let vote = parseInt(prompt('INSERISCI IL VOTO TRA 1 E 30'));
+
+if (vote>=1 && vote<=30)
+{
+    if (vote<18) {console.log('VOTO ' + vote + ' : INSUFFICIENTE');}
+    else if (vote>=18 && vote<21) {console.log('VOTO ' + vote + ' : SUFFICIENTE');}
+    else if (vote>=21 && vote<24) {console.log('VOTO ' + vote + ' : BUONO');}
+    else if (vote>=24 && vote<27) {console.log('VOTO ' + vote + ' : DISTINTO');}
+    else if (vote>=27 && vote<=29) {console.log('VOTO ' + vote + ' : OTTIMO');}
+    else {console.log('VOTO ' + vote + ' : ECCELLENTE');}
+}
+else {console.log('HAI INSERITO UN VOTO NON VALIDO');}
+*/
+
+
+let vote = parseInt(prompt('INSERISCI IL VOTO TRA 1 E 30'));
+switch (vote){
+    case 1: case 2: case 3: case 4: case 5: case 6: case 7: case 8: case 9: 
+    case 10: case 11: case 12: case 13: case 14: case 15: case 16: case 17:
+        console.log('VOTO ' + vote + ' : INSUFFICIENTE');
+    break;
+
+    case 18: case 19: case 20:
+        console.log('VOTO ' + vote + ' : SUFFICIENTE');
+    break;        
+
+    case 21: case 22: case 23:
+        console.log('VOTO ' + vote + ' : BUONO');
+    break;         
+
+    case 24: case 25: case 26:
+        console.log('VOTO ' + vote + ' : DISTINTO');
+    break;             
+
+    case 27: case 28: case 29:
+        console.log('VOTO ' + vote + ' : OTTIMO');
+    break;   
+
+    case 30:
+        console.log('VOTO ' + vote + ' : ECCELLENTE');
+    break;   
+
+    default: console.log('HAI INSERITO UN VOTO NON VALIDO')
+}
+
