78eefa3ab84dc035ca1c3e9a77c14fd817d8dedc
Andrea77g <91381365+Andrea77g@users.noreply.github.com>
2022-02-04 23:28:34 +0100

    Update caldo-o-freddo.js

--- esercizi-condizioni/caldo-o-freddo.js
+++ esercizi-condizioni/caldo-o-freddo.js
@@ -9,3 +9,40 @@
 
   http://www.imparareaprogrammare.it
 */
+
+let days =['domenica','lunedì','martedì','mercoledì','giovedì','venerdì','sabato'];
+let temp =[];
+let validation=[];
+
+for (i=0;i<7;i++) {temp[i] = parseFloat(prompt('Inserisci la temperatura di ' + days[i] + ' (da -99°C a + 99°C)'));
+                   if (temp[i]<-99 || temp[i]>99) {validation[i]=false;break;}
+};
+
+function compare(temp){ 
+let tmax, tmin; 
+  
+    if(temp[0]>temp[1]) {tmax = temp[0]; tmin = temp[1];}
+    else {tmax = temp[1]; tmin = temp[0];}
+
+    if(temp[2]>tmax) {tmax=temp[2];}
+    else if(temp[2]<tmin) {tmin=temp[2];}
+    
+    if(temp[3]>tmax) {tmax=temp[3];}
+    else if(temp[3]<tmin) {tmin=temp[3];}
+
+    if(temp[4]>tmax) {tmax=temp[4];}
+    else if(temp[4]<tmin) {tmin=temp[4];}
+
+    if(temp[5]>tmax) {tmax=temp[5];}
+    else if(temp[5]<tmin) {tmin=temp[5];}
+
+    if(temp[6]>tmax) {tmax=temp[6];}
+    else if(temp[6]<tmin) {tmin=temp[6];}
+ 
+    console.log('La giornata più calda è stata ' + days[temp.indexOf(tmax)] + ' con ' + tmax + '°C');
+    console.log('La giornata più fredda è stata ' + days[temp.indexOf(tmin)] + ' con ' + tmin + '°C');
+};
+
+if (!validation.includes(false)) {compare(temp);}
+else {console.log('Hai digitato un valore errato!');}
+
